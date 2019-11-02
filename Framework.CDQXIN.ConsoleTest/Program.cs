4a0f1992935972442703a03fcedcfcea19726939
Administrator <donchen1990@163.com>
2019-11-02 17:15:12 +0800

    添加项目文件。

---
+++ Framework.CDQXIN.ConsoleTest/Program.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.CDQXIN.ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SQLiteHandle sh = new SQLiteHandle();
+            ////增加
+            //bool flag = sh.Add(new UserInfo() { UserName = "pull", Pwd = "123456", Age = 33 });
+            //if (flag)
+            //{
+            //    Console.WriteLine("插入成功");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("插入失败");
+            //}
+
+            //查询
+            //var model=sh.GetModel(1);
+            //Console.WriteLine(JsonConvert.SerializeObject(model));
+
+            //修改
+            //UserInfo en = new UserInfo() { 
+            //ID=1,
+            //UserName="jim",
+            //Pwd="123456789",
+            //Age=19
+            //};
+            //if (sh.Update(en))
+            //{
+            //    Console.WriteLine("操作成功！");
+            //}
+            //删除
+            if(sh.Delete(1))
+            {
+                Console.WriteLine("ol");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Framework.CDQXIN.DBUtility/SQLiteHandle.cs
@@ -0,0 +1,312 @@
+﻿using Framework.CDQXIN.DBUtility;
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.CDQXIN.ConsoleTest
+{
+    public class SQLiteHandle
+    {
+
+        #region  BasicMethod
+
+        /// <summary>
+        /// 得到最大ID
+        /// </summary>
+        public int GetMaxId()
+        {
+            return DbHelperSQLite.GetMaxID("ID", "UserInfo");
+        }
+
+        /// <summary>
+        /// 是否存在该记录
+        /// </summary>
+        public bool Exists(int ID)
+        {
+            StringBuilder strSql = new StringBuilder();
+            strSql.Append("select count(1) from UserInfo");
+            strSql.Append(" where ID=@ID ");
+            SQLiteParameter[] parameters = {
+                    new SQLiteParameter("@ID", DbType.Int32,8)          };
+            parameters[0].Value = ID;
+
+            return DbHelperSQLite.Exists(strSql.ToString(), parameters);
+        }
+
+
+        /// <summary>
+        /// 增加一条数据
+        /// </summary>
+        public bool Add(UserInfo model)
+        {
+            StringBuilder strSql = new StringBuilder();
+            strSql.Append("insert into UserInfo(");
+            strSql.Append("UserName,Pwd,Age)");
+            strSql.Append(" values (");
+            strSql.Append("@UserName,@Pwd,@Age)");
+            SQLiteParameter[] parameters = {
+                    new SQLiteParameter("@UserName", DbType.String,50),
+                    new SQLiteParameter("@Pwd", DbType.String,25),
+                    new SQLiteParameter("@Age", DbType.Int32,8)};
+            parameters[0].Value = model.UserName;
+            parameters[1].Value = model.Pwd;
+            parameters[2].Value = model.Age;
+
+            int rows = DbHelperSQLite.ExecuteSql(strSql.ToString(), parameters);
+            if (rows > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// 更新一条数据
+        /// </summary>
+        public bool Update(UserInfo model)
+        {
+            StringBuilder strSql = new StringBuilder();
+            strSql.Append("update UserInfo set ");
+            strSql.Append("UserName=@UserName,");
+            strSql.Append("Pwd=@Pwd,");
+            strSql.Append("Age=@Age");
+            strSql.Append(" where ID=@ID ");
+            SQLiteParameter[] parameters = {
+                    new SQLiteParameter("@UserName", DbType.String,50),
+                    new SQLiteParameter("@Pwd", DbType.String,25),
+                    new SQLiteParameter("@Age", DbType.Int32,8),
+                    new SQLiteParameter("@ID", DbType.Int32,8)};
+            parameters[0].Value = model.UserName;
+            parameters[1].Value = model.Pwd;
+            parameters[2].Value = model.Age;
+            parameters[3].Value = model.ID;
+
+            int rows = DbHelperSQLite.ExecuteSql(strSql.ToString(), parameters);
+            if (rows > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 删除一条数据
+        /// </summary>
+        public bool Delete(int ID)
+        {
+
+            StringBuilder strSql = new StringBuilder();
+            strSql.Append("delete from UserInfo ");
+            strSql.Append(" where ID=@ID ");
+            SQLiteParameter[] parameters = {
+                    new SQLiteParameter("@ID", DbType.Int32,8)          };
+            parameters[0].Value = ID;
+
+            int rows = DbHelperSQLite.ExecuteSql(strSql.ToString(), parameters);
+            if (rows > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// 批量删除数据
+        /// </summary>
+        public bool DeleteList(string IDlist)
+        {
+            StringBuilder strSql = new StringBuilder();
+            strSql.Append("delete from UserInfo ");
+            strSql.Append(" where ID in (" + IDlist + ")  ");
+            int rows = DbHelperSQLite.ExecuteSql(strSql.ToString());
+            if (rows > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        /// <summary>
+        /// 得到一个对象实体
+        /// </summary>
+        public UserInfo GetModel(int ID)
+        {
+
+            StringBuilder strSql = new StringBuilder();
+            strSql.Append("select ID,UserName,Pwd,Age from UserInfo ");
+            strSql.Append(" where ID=@ID ");
+            SQLiteParameter[] parameters = {
+                    new SQLiteParameter("@ID", DbType.Int32,8)          };
+            parameters[0].Value = ID;
+
+            UserInfo model = new UserInfo();
+            DataSet ds = DbHelperSQLite.Query(strSql.ToString(), parameters);
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                return DataRowToModel(ds.Tables[0].Rows[0]);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        /// 得到一个对象实体
+        /// </summary>
+        public UserInfo DataRowToModel(DataRow row)
+        {
+           UserInfo model = new UserInfo();
+            if (row != null)
+            {
+                if (row["ID"] != null && row["ID"].ToString() != "")
+                {
+                    model.ID = int.Parse(row["ID"].ToString());
+                }
+                if (row["UserName"] != null)
+                {
+                    model.UserName = row["UserName"].ToString();
+                }
+                if (row["Pwd"] != null)
+                {
+                    model.Pwd = row["Pwd"].ToString();
+                }
+                if (row["Age"] != null && row["Age"].ToString() != "")
+                {
+                    model.Age = int.Parse(row["Age"].ToString());
+                }
+            }
+            return model;
+        }
+
+        /// <summary>
+        /// 获得数据列表
+        /// </summary>
+        public DataSet GetList(string strWhere)
+        {
+            StringBuilder strSql = new StringBuilder();
+            strSql.Append("select ID,UserName,Pwd,Age ");
+            strSql.Append(" FROM UserInfo ");
+            if (strWhere.Trim() != "")
+            {
+                strSql.Append(" where " + strWhere);
+            }
+            return DbHelperSQLite.Query(strSql.ToString());
+        }
+
+        /// <summary>
+        /// 获取记录总数
+        /// </summary>
+        public int GetRecordCount(string strWhere)
+        {
+            StringBuilder strSql = new StringBuilder();
+            strSql.Append("select count(1) FROM UserInfo ");
+            if (strWhere.Trim() != "")
+            {
+                strSql.Append(" where " + strWhere);
+            }
+            object obj = DbHelperSQLite.GetSingle(strSql.ToString());
+            if (obj == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return Convert.ToInt32(obj);
+            }
+        }
+        /// <summary>
+        /// 分页获取数据列表
+        /// </summary>
+        public DataSet GetListByPage(string strWhere, string orderby, int startIndex, int endIndex)
+        {
+            StringBuilder strSql = new StringBuilder();
+            strSql.Append("SELECT * FROM ( ");
+            strSql.Append(" SELECT ROW_NUMBER() OVER (");
+            if (!string.IsNullOrEmpty(orderby.Trim()))
+            {
+                strSql.Append("order by T." + orderby);
+            }
+            else
+            {
+                strSql.Append("order by T.ID desc");
+            }
+            strSql.Append(")AS Row, T.*  from UserInfo T ");
+            if (!string.IsNullOrEmpty(strWhere.Trim()))
+            {
+                strSql.Append(" WHERE " + strWhere);
+            }
+            strSql.Append(" ) TT");
+            strSql.AppendFormat(" WHERE TT.Row between {0} and {1}", startIndex, endIndex);
+            return DbHelperSQLite.Query(strSql.ToString());
+        }
+
+        #endregion  BasicMethod
+    }
+
+    /// <summary>
+    /// UserInfo:实体类(属性说明自动提取数据库字段的描述信息)
+    /// </summary>
+    [Serializable]
+    public partial class UserInfo
+    {
+        public UserInfo()
+        { }
+        #region Model
+        private int _id;
+        private string _username;
+        private string _pwd;
+        private int _age;
+        /// <summary>
+        /// 
+        /// </summary>
+        public int ID
+        {
+            set { _id = value; }
+            get { return _id; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string UserName
+        {
+            set { _username = value; }
+            get { return _username; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Pwd
+        {
+            set { _pwd = value; }
+            get { return _pwd; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Age
+        {
+            set { _age = value; }
+            get { return _age; }
+        }
+        #endregion Model
+
+    }
+}
